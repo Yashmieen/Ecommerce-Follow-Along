f043de688d154fbef889a2768a85953c1b8729b7
unknown <yashmieen1@gmail.com>
2025-03-13 15:14:31 +0530

    add task bar

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,19 +1,18 @@
 import Product from "./auth/Product";
 // import Showcase from "./auth/Showcase";
 import Navbar from "./Navbar";
+
 const Home = () => {
-  // Example list of products
-  return(
-    <div className="flex flex-col gap-10 bg-pink-400">
+  return (
+    <div className="flex flex-col gap-10 bg-pink-200">
       <div>
-    <Navbar/>
+        <Navbar />
+      </div>
+<div className="mt-15">
+      </div>
+      <Product />
     </div>
-    {/* <div className="mt-15">
-    <Showcase/>
-    </div> */}
-    <Product />
-    
-    </div>
-  )
-}
+  );
+};
+
 export default Home;
